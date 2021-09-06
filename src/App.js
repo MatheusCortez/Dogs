9bfd311ac233f7bd3628c8aaa1516a9f29b4e153
Matheus Cortez <matheus.cortez@live.com>
2021-09-06 11:39:23 -0300

    feat: page foto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './styles/App/app.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Footer from './pages/Components/Footer/Footer';
 import Header from './pages/Components/Header/Header'
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+
 import {UserStorage} from './Hooks/Context/UserContext';
 import User from './pages/User/User';
 import ProtectedRoute from './pages/Components/Helpers/ProtectedRoute/ProtectedRoute';
-import UserProfile from './pages/User/Profile/UserProfile';
+import Photo from './pages/Components/Photo/Photo';
+
 function App() {
   return (
     <BrowserRouter className="App">
@@ -19,8 +22,7 @@ function App() {
         <Route path="/login/*" element={<Login/>} />
        
         <ProtectedRoute path="/conta/*" element={<User/>}/>
-
-        <ProtectedRoute path="/perfil/:user" element={<UserProfile/>}/>
+        <ProtectedRoute path="/foto/:id" element={<Photo/>}/>
       </Routes>
     <Footer/>
     </UserStorage>
--- src/pages/Components/Photo/Photo.jsx
+++ src/pages/Components/Photo/Photo.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import { useParams } from 'react-router'
+
+import { PHOTO_GET} from '../../../API/api';
+import useFetch from '../../../Hooks/useFetch';
+import Error from '../Helpers/Errors/error'
+import Loading from '../Helpers/Loading/Loading'
+import PhotoContent from './PhotoContent/PhotoContent';
+
 const Photo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+    const {id} = useParams();
+    const {data,loading,error,request} = useFetch();
+
+    React.useEffect(()=>{
+
+        const {url,options} = PHOTO_GET(id)
+        request(url,options)
+       
+    },[id,request])
+  
+    if (error) return <Error error={error} />;
+    if (loading) return <Loading />;
+    if(data)  return <section> <PhotoContent single={true}  data={data} /></section>
+    else return null;
+    
 }
 
 export default Photo
